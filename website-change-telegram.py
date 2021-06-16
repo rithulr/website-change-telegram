94156fd263bde9cf6b3b4895d4c7c175736fd66f
NoBody <nobodyhailearth@gmail.com>
2021-06-16 07:32:51 +0530

    Modified branch

--- website-change-telegram.py
+++ website-change-telegram.py
@@ -11,8 +11,8 @@ import time
 
 # Send a message via a telegram bot
 def telegram_bot_sendtext(bot_message):
-    bot_token = 'YOUR_BOT_TOKEN'
-    bot_chatID = 'TELEGRAM_USER_CHAT_ID'
+    bot_token = '1862933508:AAGSvwSfigHixCrAnMLS2JSOONGQx1BxuX0'
+    bot_chatID = 'CanadaVACIndiaBot'
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
